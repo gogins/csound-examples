aba26db9d70864b5cb41bf2cfabdb4c9bb454b1d
Michael Gogins <michael.gogins@gmail.com>
2021-01-02 15:52:11 -0500

    Adding orbifold explorer (#148).

--- local-update-dependencies.sh
+++ local-update-dependencies.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 echo "Updating WebAssemby dependencies for csound-examples from my home directory..."
 
-cp ~/csound-extended/docs/cmask.js csound-extended-wasm/cmask.js
-cp ~/csound-extended/docs/cmask.wasm csound-extended-wasm/cmask.wasm
-cp ~/csound-extended/docs/csound_loader.js csound-extended-wasm/csound_loader.js
-cp ~/csound-extended/docs/csound_samples.js csound-extended-wasm/csound_samples.js
-cp ~/csound-extended/docs/CsoundAC.js csound-extended-wasm/CsoundAC.js
-cp ~/csound-extended/docs/CsoundAudioNode.js csound-extended-wasm/CsoundAudioNode.js
-cp ~/csound-extended/docs/CsoundAudioProcessor.js csound-extended-wasm/CsoundAudioProcessor.js
-cp ~/csound-extended/docs/CsoundAudioProcessor.wasm csound-extended-wasm/CsoundAudioProcessor.wasm
-cp ~/csound-extended/docs/piano-roll.js csound-extended-wasm/piano-roll.js
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.aif ./csound-extended-wasm/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.data ./csound-extended-wasm/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.inc ./csound-extended-wasm/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.js ./csound-extended-wasm/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.map ./csound-extended-wasm/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.wasm ./csound-extended-wasm/
 
-cp ~/csound-extended/docs/cmask.js csound-for-android/Gogins/cmask.js
-cp ~/csound-extended/docs/cmask.wasm csound-for-android/Gogins/cmask.wasm
-cp ~/csound-extended/docs/csound_loader.js csound-for-android/Gogins/csound_loader.js
-cp ~/csound-extended/docs/csound_samples.js csound-for-android/Gogins/csound_samples.js
-cp ~/csound-extended/docs/CsoundAC.js csound-for-android/Gogins/CsoundAC.js
-cp ~/csound-extended/docs/CsoundAudioNode.js csound-for-android/Gogins/CsoundAudioNode.js
-cp ~/csound-extended/docs/CsoundAudioProcessor.js csound-for-android/Gogins/CsoundAudioProcessor.js
-cp ~/csound-extended/docs/CsoundAudioProcessor.wasm csound-for-android/Gogins/CsoundAudioProcessor.wasm
-cp ~/csound-extended/docs/piano-roll.js csound-for-android/Gogins/piano-roll.js
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.aif ./csound-for-android/Gogins/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.data ./csound-for-android/Gogins/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.inc ./csound-for-android/Gogins/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.js ./csound-for-android/Gogins/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.map ./csound-for-android/Gogins/
+cp -rf ~/csound-extended/WebAssembly/csound-extended-wasm-1.1.0/*.wasm ./csound-for-android/Gogins/
 
 find . -name "*.js" -ls
 find . -name "*.wasm" -ls
